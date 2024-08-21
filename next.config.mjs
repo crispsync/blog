c6bf346d225b2c9975c4806f4d100fc528e8591f
crispsync <crispsync@outlook.com>
2024-08-21 22:56:22 +0400

    chore: change config for prod

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,12 @@ let basePath = '';
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
   assetPrefix = `/${repo}/`;
-  basePath = ``;
+  basePath = `/${repo}`;
 } else {
   // Manually set the repository name
-  const repo = ''; // This should match your repository name
+  const repo = 'blog'; // This should match your repository name
   assetPrefix = `/${repo}/`;
-  basePath = ``;
+  basePath = `/${repo}${}
 }
 
 export default {
@@ -21,5 +21,5 @@ export default {
   images: {
     unoptimized: true,
   },
-//   output: 'export',
+  output: 'export',
 };
